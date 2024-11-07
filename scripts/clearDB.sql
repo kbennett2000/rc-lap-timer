e188a5cd4fbccdea6d738e1ac9a0fca809a4766c
kbennett2000 <kbennett2000@hotmail.com>
2024-11-06 20:45:02 -0700

    script edit - add db name to SQL statements

--- scripts/clearDB.sql
+++ scripts/clearDB.sql
@@ -5,14 +5,14 @@
 SET FOREIGN_KEY_CHECKS = 0;
 
 -- Clear all tables
-TRUNCATE TABLE SessionRequest;
-TRUNCATE TABLE Penalty;
-TRUNCATE TABLE Lap;
-TRUNCATE TABLE Session;
-TRUNCATE TABLE Car;
-TRUNCATE TABLE Driver;
-TRUNCATE TABLE Location;
-TRUNCATE TABLE MotionSettings;
+TRUNCATE TABLE rc_lap_timer.SessionRequest;
+TRUNCATE TABLE rc_lap_timer.Penalty;
+TRUNCATE TABLE rc_lap_timer.Lap;
+TRUNCATE TABLE rc_lap_timer.Session;
+TRUNCATE TABLE rc_lap_timer.Car;
+TRUNCATE TABLE rc_lap_timer.Driver;
+TRUNCATE TABLE rc_lap_timer.Location;
+TRUNCATE TABLE rc_lap_timer.MotionSettings;
 
 -- Re-enable foreign key checks
 SET FOREIGN_KEY_CHECKS = 1;
